4bc736c5b64cdca06e9f85fd5ad1f5fb4789d1a8
Sumeyye <sumeyyedeg00@gmail.com>
2023-10-03 00:29:26 +0300

    Home page designed, difficulty levels added.

---
+++ src/pages/introduce/Introduce.jsx
@@ -0,0 +1,28 @@
+import React, { useState } from 'react'
+import './Introduce.css'
+import Dropdown from '../../components/dropdown/Dropdown'
+import { useNavigate } from 'react-router-dom'
+
+const Introduce = () => {
+    const difficulty=["easy","medium","hard"]
+    const [difficultyChange,setDifficultyChange]=useState('')
+    const navigate=useNavigate()
+    const TOTAL_QUESTİONS=10
+
+    const startQuiz=()=>{
+        if(difficultyChange){
+            navigate(`/quiz/${difficultyChange}/${TOTAL_QUESTİONS}`)
+        }
+    }
+  return (
+    <div className='introduce'>
+        <div className="introduce-container">
+            <img src='https://img.freepik.com/premium-vector/quiz-time_690577-160.jpg' alt="" />
+            <Dropdown data={difficulty} setDifficultyChange={setDifficultyChange}/>
+            <div onClick={startQuiz} className='introduce-btn'>Başla</div>
+        </div>
+    </div>
+  )
+}
+
+export default Introduce
